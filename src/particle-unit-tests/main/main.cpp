28cab50ba0a2be2fed80e3cdb11278f320fad1d5
Raoul Rubien <rubienr@gmail.com>
2015-03-05 17:17:39 +0100

    fixed include error

--- src/particle-unit-tests/main/main.cpp
+++ src/particle-unit-tests/main/main.cpp
@@ -4,25 +4,13 @@
 #include <avr/interrupt.h>
 
 #include <common/common.h>
-#include "PortDefinition.h"
 
-extern "C" {
-
-}
-;
-
-/**
- * free function declarations
- */
 void init(void);
 
 int main(void) {
+    init();
     return 0;
 }
 
-/**
- * initialization before main is called
- */
 void init() {
 }
-
