fdac77c1fbed036c7144fbf0df646b8b5f8661eb
Raoul Rubien <rubienr@gmail.com>
2016-03-03 13:35:27 +0100

    replaced/symlinked scripts folders since it caused troubles with clion-cmake

--- src/avr/avr-common/scripts/post_binary_build.cmake
+++ src/avr/avr-common/scripts/post_binary_build.cmake
@@ -5,18 +5,19 @@
 
 
 # symlinking binary for convenience (independend if target is up to date or not)
-execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-         COMMAND ln -sfT ${CMAKE_CURRENT_BINARY_DIR}/${BINARY} ${CMAKE_BINARY_DIR}/${BINARY}
-#        COMMAND ln -sf ${CMAKE_CURRENT_BINARY_DIR}/${BINARY} ${CMAKE_CURRENT_BINARY_DIR}/main.elf
-         RESULT_VARIABLE ret_var
-         OUTPUT_VARIABLE output_var
-)
+#execute_process(WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+#         COMMAND ln -sfT ${CMAKE_CURRENT_BINARY_DIR}/${BINARY} ${CMAKE_BINARY_DIR}/${BINARY}
+##        COMMAND ln -sf ${CMAKE_CURRENT_BINARY_DIR}/${BINARY} ${CMAKE_CURRENT_BINARY_DIR}/main.elf
+#         RESULT_VARIABLE ret_var
+#         OUTPUT_VARIABLE output_var
+#)
 
 
 # show stats about binary
 add_custom_command (TARGET ${BINARY}
          POST_BUILD
          WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+         #COMMAND ln -sfT ${CMAKE_CURRENT_BINARY_DIR}/${BINARY} ${CMAKE_BINARY_DIR}/${BINARY}
          COMMENT "${BINARY} built for [${CMCU}]@[${CDEFS}]:"
          COMMAND echo "MD5" && md5sum ./${BINARY} | cut -d' ' -f1
          COMMAND echo "filesize on system" && ls -l ./${BINARY} | cut -d' ' -f5 && echo ""
