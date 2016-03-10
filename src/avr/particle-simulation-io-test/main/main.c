810056f220c3b38eab1c6eabdb2950a1f5eabfbd
Raoul Rubien <rubienr@gmail.com>
2016-03-10 16:02:15 +0100

    added test-able fw. for testing all i/o pins config in fw. and simulation

---
+++ src/avr/particle-simulation-io-test/main/main.c
@@ -0,0 +1,129 @@
+/**
+ * @author Raoul Rubien 2016
+ */
+
+#include <uc-core/ParticleIoDefinitions.h>
+#include <common/common.h>
+
+/**
+ * Generates a certain amount of transitions/writes per output wires/registers. The expected amount of
+ * transitions are monitored and evaluated in the simulation by test cases.
+ */
+int main(void) {
+    int extraTransitions = 2;
+
+    extraTransitions++;
+    LED_STATUS0_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        LED_STATUS0_OFF;
+        LED_STATUS0_ON;
+    }
+
+    extraTransitions++;
+    LED_STATUS1_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        LED_STATUS1_OFF;
+        LED_STATUS1_ON;
+    }
+
+    extraTransitions++;
+    LED_HEARTBEAT_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        LED_HEARTBEAT_OFF;
+        LED_HEARTBEAT_ON;
+    }
+
+    extraTransitions++;
+    LED_ERROR_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        LED_ERROR_OFF;
+        LED_ERROR_ON;
+    }
+
+    extraTransitions++;
+    TEST_POINT1_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        TEST_POINT1_LO;
+        TEST_POINT1_HI;
+    }
+
+    extraTransitions++;
+    TEST_POINT2_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        TEST_POINT2_LO;
+        TEST_POINT2_HI;
+    }
+
+    extraTransitions++;
+    TEST_POINT3_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        TEST_POINT3_LO;
+        TEST_POINT3_HI;
+    }
+
+    extraTransitions++;
+    NORTH_TX_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        NORTH_TX_LO;
+        NORTH_TX_HI;
+    }
+
+    extraTransitions++;
+    NORTH_RX_SWITCH_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        NORTH_RX_SWITCH_LO;
+        NORTH_RX_SWITCH_HI;
+    }
+
+    extraTransitions++;
+    SOUTH_TX_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        SOUTH_TX_LO;
+        SOUTH_TX_HI;
+    }
+
+    extraTransitions++;
+    SOUTH_RX_SWITCH_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        SOUTH_RX_SWITCH_LO;
+        SOUTH_RX_SWITCH_HI;
+    }
+
+    extraTransitions++;
+    EAST_TX_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        EAST_TX_LO;
+        EAST_TX_HI;
+    }
+
+    extraTransitions++;
+    EAST_RX_SWITCH_SETUP;
+    for (int i = 0; i < extraTransitions; ++i) {
+        EAST_RX_SWITCH_LO;
+        EAST_RX_SWITCH_HI;
+    }
+
+    int writes = extraTransitions;
+    writes++;
+    NORTH_RX_SETUP;
+    for (int i = 0; i < writes; ++i) {
+        NORTH_RX_PULL_DOWN;
+        NORTH_RX_PULL_UP;
+    }
+
+    writes++;
+    SOUTH_RX_SETUP;
+    for (int i = 0; i < writes; ++i) {
+        SOUTH_RX_PULL_DOWN;
+        SOUTH_RX_PULL_UP;
+    }
+
+    writes++;
+    EAST_RX_SETUP;
+    for (int i = 0; i < writes; ++i) {
+        EAST_RX_PULL_DOWN;
+        EAST_RX_PULL_UP;
+    }
+
+    forever { };
+}
--- src/avr/particle-simulation/main/main.c
+++ src/avr/particle-simulation/main/main.c
@@ -2,33 +2,8 @@
  * @author Raoul Rubien 2015
  */
 
-#include <uc-core/ParticleIoDefinitions.h>
-#include <common/common.h>
+#include <uc-core/ParticleMainLoop.h>
 
 int main(void) {
-//    return particleLoop();
-
-    LED_STATUS0_SETUP;
-//    LED_STATUS1_SETUP;
-//    LED_HEARTBEAT_SETUP;
-//    LED_ERROR_SETUP;
-//
-//    TEST_POINT1_SETUP;
-//    TEST_POINT2_SETUP;
-//    TEST_POINT3_SETUP;
-//
-//    NORTH_TX_SETUP;
-//    NORTH_RX_SETUP;
-//    NORTH_RX_SWITCH_SETUP;
-//
-//    SOUTH_TX_SETUP;
-//    SOUTH_RX_SETUP;
-//    SOUTH_RX_SWITCH_SETUP;
-//
-//    EAST_TX_SETUP;
-//    EAST_RX_SETUP;
-//    EAST_RX_SWITCH_SETUP;
-
-    forever { };
-
+    return particleLoop();
 }
