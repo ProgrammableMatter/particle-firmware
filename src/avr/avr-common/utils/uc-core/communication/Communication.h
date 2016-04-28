d5392a7fc7c7fea3db3164ec30ce158c599f0584
Raoul Rubien <rubienr@gmail.com>
2016-04-28 19:41:54 +0200

    reception can now receive 4 bytes at once

--- src/avr/avr-common/utils/uc-core/communication/Communication.h
+++ src/avr/avr-common/utils/uc-core/communication/Communication.h
@@ -19,21 +19,21 @@
 /**
  * Translates the hardware counter to a specific port counter according to the provided TimerCounterAdjustment.
  */
-FUNC_ATTRS uint16_t __toPortCounter(const uint16_t *hardwareCounter,
-                                    volatile TimerCounterAdjustment *portTimerArguments) {
-    uint16_t accumulatedOffset = *hardwareCounter + portTimerArguments->receptionOffset;
-    if (accumulatedOffset > TIMER_TX_RX_COMPARE_TOP_VALUE) {
-        return accumulatedOffset - TIMER_TX_RX_COMPARE_TOP_VALUE;
+FUNC_ATTRS uint16_t __toPortCounter(const volatile uint16_t *hardwareCounter,
+                                    const volatile TimerCounterAdjustment *portTimerArguments) {
+    uint16_t portCounter = *hardwareCounter + portTimerArguments->receptionOffset;
+    if (portCounter >= TIMER_TX_RX_COMPARE_TOP_VALUE) {
+        return portCounter - TIMER_TX_RX_COMPARE_TOP_VALUE;
     }
-    return accumulatedOffset;
+    return portCounter;
 }
 
 /**
  * Stores the data bit to the reception buffer unless the buffer is saturated.
  */
-FUNC_ATTRS void __storeDataBit(volatile RxPort *rxPort, volatile uint8_t isRisingEdge) {
+FUNC_ATTRS void __storeDataBit(volatile RxPort *rxPort, const volatile uint8_t isRisingEdge) {
 
-    if (isBufferFull(&(rxPort->buffer.pointer)) == false) {
+    if (isRxBufferFull(&(rxPort->buffer.pointer)) == false) {
         // save bit to buffer
         if (isRisingEdge == 0) {
             IF_SIMULATION_CHAR_OUT('0');
@@ -42,81 +42,77 @@ FUNC_ATTRS void __storeDataBit(volatile RxPort *rxPort, volatile uint8_t isRisin
             IF_SIMULATION_CHAR_OUT('1');
             rxPort->buffer.bytes[rxPort->buffer.pointer.byteNumber] setBit rxPort->buffer.pointer.bitMask;
         }
-        // increment pointer
-        if (rxPort->buffer.pointer.bitMask != (1 << 7)) {
-            rxPort->buffer.pointer.bitMask <<= 1;
-        } else {
-            rxPort->buffer.pointer.bitMask = 1;
-            rxPort->buffer.pointer.byteNumber++;
-        }
+        rxBufferBitPointerNext(&(rxPort->buffer.pointer));
     }
 }
 
 /**
- * Resets the counter. In case the interrupt was shifted (less than 1x compare value), the counter is trimmed,
- * else in case of simulation it switches to erroneous state.
+ * In case the counter is greater than the compare value (i.e. compare interrupt was shifted) returns
+ * the amount counted over else the timer.
  */
-FUNC_ATTRS void resetReceptionCounter(void) {
-#ifdef SIMULATION
-    if (TIMER_TX_RX_COUNTER > (2 * TIMER_TX_RX_COMPARE_TOP_VALUE)) {
-        IF_SIMULATION_SWITCH_TO_ERRONEOUS_STATE;
-    }
-#endif
-    if (TIMER_TX_RX_COUNTER > TIMER_TX_RX_COMPARE_TOP_VALUE) {
-        TIMER_TX_RX_COUNTER -= TIMER_TX_RX_COMPARE_TOP_VALUE;
-    } else {
-        TIMER_TX_RX_COUNTER = 0;
-    }
-}
+FUNC_ATTRS uint16_t __getTrimmedReceptionCounter(void) {
 
+//#ifdef SIMULATION
+//    if (TIMER_TX_RX_COUNTER > (2 * TIMER_TX_RX_COMPARE_TOP_VALUE)) {
+//        IF_SIMULATION_CHAR_OUT('R');
+//        IF_SIMULATION_SWITCH_TO_ERRONEOUS_STATE;
+//    }
+//#endif
+
+    if (TIMER_TX_RX_COUNTER > TIMER_TX_RX_COMPARE_TOP_VALUE) {
+        return TIMER_TX_RX_COUNTER - TIMER_TX_RX_COMPARE_TOP_VALUE;
+    }
+    return TIMER_TX_RX_COUNTER;
+}
 
 /**
  * Handles received data flanks and stores them according to the received time relative to the reception
  * timer/counter.
  */
-FUNC_ATTRS void dispatchReceivedDataEdge(volatile RxPort *rxPort, volatile uint16_t *receptionDelta,
-                                         uint8_t isRisingEdge) {
-    uint16_t hardwareCounter = TIMER_TX_RX_COUNTER; //__getTrimmedCounter();
-    IF_SIMULATION_INT16_OUT(hardwareCounter);
+FUNC_ATTRS void dispatchReceivedDataEdge(volatile RxPort *rxPort, const volatile uint16_t *receptionDelta,
+                                         const uint8_t isRisingEdge) {
+    uint16_t hardwareCounter = __getTrimmedReceptionCounter(); // TIMER_TX_RX_COUNTER;
+//    IF_SIMULATION_INT16_OUT(hardwareCounter);
+    uint16_t captureCounter = __toPortCounter(&hardwareCounter, &(rxPort->adjustment));
+    IF_SIMULATION_INT16_OUT(captureCounter);
 
     // if there is no ongoing reception thus this this call is the first signal of a package
-    if (rxPort->isReceiving == 0) {
+    if (isNotReceiving(rxPort)) {
         if (isRisingEdge == false) {
             IF_SIMULATION_CHAR_OUT('S');
             // synchronize the counter for this channel by using an offset
             rxPort->adjustment.receptionOffset = TIMER_TX_RX_COMPARE_TOP_VALUE - hardwareCounter; // + 9;
-            rxPort->isReceiving = 0b111;
+            rxPort->isReceiving = 0b1111;
         }
     }
     else { // reception is ongoing thus this signal belongs to the current emission
-
         // reconstruct the synchronized counter
-        uint16_t captureCounter = __toPortCounter(&hardwareCounter, &(rxPort->adjustment));
         // if signal occurs approx. at 1/2 of a package clock:
-        if ((rxPort->adjustment.leftOfCenter <= captureCounter) &&
-            (captureCounter <= rxPort->adjustment.rightOfCenter)) {
+        if ((TX_RX_RECEPTION_VALUE_LEFT_OF_CENTER <= captureCounter) &&
+            (captureCounter <= TX_RX_RECEPTION_VALUE_RIGHT_OF_CENTER)) {
+
+            // re-adjust reception offset
+            rxPort->adjustment.receptionOffset =
+                    TIMER_TX_RX_COMPARE_TOP_VALUE - hardwareCounter + TX_RX_RECEPTION_VALUE_CENTER;
             IF_SIMULATION_CHAR_OUT('B');
             __storeDataBit(rxPort, isRisingEdge);
-
-            rxPort->isReceiving = 0b111;
         }
 
         else // if signal occurs approx. at the end/beginning of a package clock:
-        if (((rxPort->adjustment.leftOfTop <= captureCounter) &&
-             (captureCounter <= TIMER_TX_RX_COMPARE_TOP_VALUE)) ||
-            (captureCounter <= *receptionDelta)) {
+        {
+            // re-adjust reception offset
+            rxPort->adjustment.receptionOffset = TIMER_TX_RX_COMPARE_TOP_VALUE - hardwareCounter;
+
             IF_SIMULATION_CHAR_OUT('A');
-            rxPort->isReceiving = 0b111;
         }
-#  ifdef IS_SIMULATION
-        else {
-            IF_SIMULATION_CHAR_OUT('x');
-            IF_SIMULATION_SWITCH_TO_ERRONEOUS_STATE;
-        }
-#  endif
+        rxPort->isReceiving = 0b1111;
+//#  ifdef IS_SIMULATION
+//        else {
+//            IF_SIMULATION_CHAR_OUT('x');
+//            IF_SIMULATION_SWITCH_TO_ERRONEOUS_STATE;
+//        }
+//#  endif
     }
-//    resetReceptionCounter();
-//    TIMER_TX_RX_COUNTER_CLEAR_PENDING_INTERRUPTS;
 }
 
 
--- src/avr/avr-common/utils/uc-core/communication/CommunicationTypes.h
+++ src/avr/avr-common/utils/uc-core/communication/CommunicationTypes.h
@@ -48,27 +48,6 @@ FUNC_ATTRS void constructPortBuffer(volatile PortBuffer *o) {
     constructBufferBitPointer(&(o->pointer));
 }
 
-/**
- * returns true if the pointer points at the very first position, else false
- */
-FUNC_ATTRS uint8_t isBufferEmpty(volatile BufferBitPointer *bufferPointer) {
-    if (bufferPointer->bitMask == 0 && bufferPointer->byteNumber == 0) {
-        return true;
-    }
-    return false;
-}
-
-/**
- * returns true if the pointer points at the very last position, else false
- */
-FUNC_ATTRS uint8_t isBufferFull(volatile BufferBitPointer *bufferPointer) {
-    if (bufferPointer->bitMask == (1 << 7) &&
-        bufferPointer->byteNumber == sizeof(((PortBuffer *) 0)->bytes)) {
-        return true;
-    }
-    return false;
-}
-
 typedef struct {
     PortBuffer buffer;
 } TxPort; // 6 bytes total
@@ -89,33 +68,23 @@ FUNC_ATTRS void constructTxPorts(volatile TxPorts *o) {
     constructTxPort(&(o->south));
 }
 
-/*
-typedef struct {
-    uint16_t receptionOffset; // synchronization offset of fist received bit relative to compare counter
-    uint16_t estimatedCounterTop; // the estimated reception counter's top value
-    uint16_t estimatedCounterCenter;
-    uint16_t receptionDelta; // time span for reception classification: center|top +/- delta
-    int8_t estimatedTopCounterOffset; // an approximated counter offset
-} TimerCounterAdjustment; // 2 + 1 + (+ 1) = 4 bytes total
-*/
-
 typedef struct {
     uint16_t receptionOffset; // synchronization offset of fist received bit relative to compare counter
     uint16_t center; // center, usually DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_COUNTER_CENTER_VALUE_DIVISOR
     uint16_t leftOfCenter; // left border of center classification
     uint16_t rightOfCenter; // right border of center classification
+    // TODO: remove left of top
     uint16_t leftOfTop; // left border of top classification
     // top equals to TIMER_TX_RX_COMPARE_TOP_VALUE
-    // rightOfTop equals to 0+receptionDelta
 } TimerCounterAdjustment;
 
 FUNC_ATTRS void constructTimerCounterAdjustment(volatile TimerCounterAdjustment *o,
-                                                volatile uint16_t *receptionDelta) {
+                                                const volatile uint16_t *receptionDelta) {
     o->receptionOffset = 0;
-    o->center = DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_COUNTER_CENTER_VALUE_DIVISOR;
+    o->center = (DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_COUNTER_CENTER_VALUE_DIVISOR);
     o->leftOfCenter = o->center - *receptionDelta;
     o->rightOfCenter = o->center + *receptionDelta;
-    o->leftOfTop = DEFAULT_TX_RX_COMPARE_TOP_VALUE - *receptionDelta;
+    o->leftOfTop = o->rightOfCenter + 1;
 }
 
 typedef struct {
@@ -132,6 +101,13 @@ FUNC_ATTRS void constructRxPort(volatile RxPort *o, volatile uint16_t *reception
     o->isReceiving = 0;
 }
 
+/**
+ * returns false on ongoing reception else true
+ */
+FUNC_ATTRS bool isNotReceiving(volatile RxPort *o) {
+    return o->isReceiving == 0;
+}
+
 typedef struct {
     uint16_t receptionDelta;
     RxPort north;
@@ -140,7 +116,7 @@ typedef struct {
 } RxPorts; // 3 * 11 = 33 bytes total
 
 FUNC_ATTRS void constructRxPorts(volatile RxPorts *o) {
-    o->receptionDelta = DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_RECEPTION_DELTA_VALUE_DIVISOR - 1;
+    o->receptionDelta = DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_RECEPTION_DELTA_VALUE_DIVISOR;
     constructRxPort(&(o->north), &(o->receptionDelta));
     constructRxPort(&(o->east), &(o->receptionDelta));
     constructRxPort(&(o->south), &(o->receptionDelta));
@@ -156,9 +132,112 @@ FUNC_ATTRS void constructPorts(volatile Ports *o) {
     constructRxPorts(&(o->rx));
 }
 
+/**
+ * Decrements the bit mask and the byte number accordingly. Does not verify underflow.
+ */
+// TODO: should decrement the pointer until mask == 0 and byte number == 0
+FUNC_ATTRS void txBufferBitPointerNext(volatile BufferBitPointer *o) {
+    o->bitMask >>= 1;
+    if (o->bitMask == 0) {
+        o->bitMask = 0x80;
+        o->byteNumber--;
+    }
+}
 
-#include "../ParticleParameters.h"
+/**
+ * Increments the bit mask and the buyte number accordingly. Does not verify the buffer boundary.
+ */
+FUNC_ATTRS void rxBufferBitPointerNext(volatile BufferBitPointer *o) {
+    o->bitMask <<= 1;
+    if (o->bitMask == 0) {
+        o->bitMask = 1;
+        o->byteNumber++;
+    }
+}
 
+inline void __setToLowestPosition(volatile BufferBitPointer *o) {
+    o->bitMask = 1;
+    o->byteNumber = 0;
+}
+
+inline void __setToHighestPosition(volatile BufferBitPointer *o) {
+    o->bitMask = 0x80;
+    o->byteNumber = (sizeof(((PortBuffer *) 0)->bytes) - 1);
+}
+
+/**
+ * points the reception buffer pointer to the start position (lowest bit)
+ */
+FUNC_ATTRS void rxBufferBitPointerStart(volatile BufferBitPointer *o) {
+    __setToLowestPosition(o);
+}
+
+
+/**
+ * points the reception buffer pointer to the last position (lowest bit)
+ */
+FUNC_ATTRS void rxBufferBitPointerEnd(volatile BufferBitPointer *o) {
+    __setToHighestPosition(o);
+}
+
+/**
+ * points the reception buffer pointer to the start position (lowest bit)
+ */
+FUNC_ATTRS void txBufferBitPointerStart(volatile BufferBitPointer *o) {
+    __setToHighestPosition(o);
+}
+
+/**
+ * points the reception buffer pointer to the last position (lowest bit)
+ */
+FUNC_ATTRS void txBufferBitPointerEnd(volatile BufferBitPointer *o) {
+    __setToLowestPosition(o);
+}
+
+inline bool __isBufferFull(volatile BufferBitPointer *o) {
+    return (o->byteNumber + 1 > sizeof(((PortBuffer *) 0)->bytes));
+}
+
+inline bool __isBufferEnd(volatile BufferBitPointer *o) {
+    return (o->bitMask == (1 << 7) &&
+            o->byteNumber + 1 == sizeof(((PortBuffer *) 0)->bytes));
+}
+
+inline bool __isBufferStart(volatile BufferBitPointer *o) {
+    return (o->bitMask == 1 && o->byteNumber == 0);
+}
+
+inline bool __isBufferEmpty(volatile BufferBitPointer *o) {
+    return (o->bitMask == 0 && o->byteNumber == 0);
+}
+
+/**
+ * returns true if the pointer points at no position
+ */
+FUNC_ATTRS bool isRxBufferEmpty(volatile BufferBitPointer *o) {
+    return __isBufferEmpty(o);
+}
+
+/**
+ * returns true if the pointer points at the beyond the maximum buffer position
+ */
+FUNC_ATTRS bool isRxBufferFull(volatile BufferBitPointer *o) {
+    return __isBufferFull(o);
+}
+
+/**
+ * returns true if the buffer points to no position
+ */
+FUNC_ATTRS bool isTxBufferEmpty(volatile BufferBitPointer *o) {
+    return __isBufferEmpty(o);
+}
+
+/**
+ * returns true if the pointer points beyond the maximum buffer position
+ */
+FUNC_ATTRS bool isTxBufferFull(volatile BufferBitPointer *o) {
+    return __isBufferFull(o);
+}
 
 #  ifdef FUNC_ATTRS
 #    undef FUNC_ATTRS
