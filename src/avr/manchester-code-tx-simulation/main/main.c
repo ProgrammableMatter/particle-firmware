bffe5a3963a5ae4d789bf0fe00a501344dd2ac5d
Raoul Rubien <rubienr@gmail.com>
2016-04-29 15:25:58 +0200

    refined timing dependency

--- src/avr/manchester-code-tx-simulation/main/main.c
+++ src/avr/manchester-code-tx-simulation/main/main.c
@@ -8,13 +8,15 @@
 #include <uc-core/IoDefinitions.h>
 #include <uc-core/Globals.h>
 #include <util/delay.h>
+//#include <simulation/SimulationMacros.h>
 
 extern volatile ParticleState ParticleAttributes;
 //uint8_t bitMask  __attribute__((section(".noinit")));
 //unsigned char__stuff __attribute__((section(".noinit")));
 
-#define COUNTER_1_SETTINGS_TOP 750
-#define COUNTER_1_SETTINGS_CENTER (COUNTER_1_SETTINGS_TOP / 2)
+#define COUNTER_1_SETTINGS_TOP_INTERRUPT_LATENCY 18
+#define COUNTER_1_SETTINGS_TOP (DEFAULT_TX_RX_COMPARE_TOP_VALUE - COUNTER_1_SETTINGS_TOP_INTERRUPT_LATENCY)
+#define COUNTER_1_SETTINGS_CENTER (((COUNTER_1_SETTINGS_TOP+COUNTER_1_SETTINGS_TOP_INTERRUPT_LATENCY) / 2) - COUNTER_1_SETTINGS_TOP_INTERRUPT_LATENCY)
 #define COUNTER_1_SETTINGS_PRESCALER ((0 << CS12) | (0 << CS11) | (1 << CS10))
 #define COUNTER_1_SETTINGS_PRESCALER_DISCONNECT ((1 << CS12) | (1 << CS11) | (1 << CS10))
 
@@ -22,18 +24,16 @@ extern volatile ParticleState ParticleAttributes;
 // triggers on signal clock (signal rectification)
 ISR(TIMER1_COMPA_vect) {
     TCNT1 = 0;
-//    if (isTxBufferEmpty(&ParticleAttributes.ports.tx.south.buffer.pointer)) {
-//        // stop after one transmission
-//        TIMSK unsetBit(1 << OCIE1A);
-//    } else {
-        // rectify (invert according to next data bit) signal before upcoming data transition
+    // rectify (invert according to next data bit) signal before upcoming data transition
     //if (ParticleAttributes.ports.tx.south.buffer.pointer.bitMask & ParticleAttributes.ports.tx.south.buffer.bytes[0]) {
     if (ParticleAttributes.ports.tx.south.buffer.pointer.bitMask &
         ParticleAttributes.ports.tx.south.buffer.bytes[ParticleAttributes.ports.tx.
                 south.buffer.pointer.byteNumber]) {
             SOUTH_TX_HI;
+//            IF_SIMULATION_INT16_OUT(TCNT1);
         } else {
             SOUTH_TX_LO;
+//            IF_SIMULATION_INT16_OUT(TCNT1);
         }
 //    }
 }
@@ -48,16 +48,18 @@ ISR(TIMER1_COMPB_vect) {
         TCCR1B unsetBit COUNTER_1_SETTINGS_PRESCALER_DISCONNECT;
         // return signal to default
         SOUTH_TX_LO; // inverted on receiver side
+//        IF_SIMULATION_INT16_OUT(TCNT1);
         ParticleAttributes.node.state = STATE_TYPE_TX_DONE;
     } else {
         // write data bit to output (inverted)
         if (ParticleAttributes.ports.tx.south.buffer.pointer.bitMask &
             ParticleAttributes.ports.tx.south.buffer.bytes[ParticleAttributes.ports.tx.south.buffer.pointer.byteNumber]) {
             SOUTH_TX_LO;
+//            IF_SIMULATION_INT16_OUT(TCNT1);
         } else {
             SOUTH_TX_HI;
+//            IF_SIMULATION_INT16_OUT(TCNT1);
         }
-        //ParticleAttributes.ports.tx.south.buffer.pointer.bitMask <<= 1;
         txBufferBitPointerNext(&ParticleAttributes.ports.tx.south.buffer.pointer);
     }
 }
@@ -73,7 +75,7 @@ inline void initTransmission(void) {
     ParticleAttributes.ports.tx.south.buffer.bytes[3] = 0b10100111;
     ParticleAttributes.ports.tx.south.buffer.bytes[2] = 0xaa;
     ParticleAttributes.ports.tx.south.buffer.bytes[1] = 0xaa;
-    ParticleAttributes.ports.tx.south.buffer.bytes[0] = 0xaa; //0b10100111;
+    ParticleAttributes.ports.tx.south.buffer.bytes[0] = 0b10100111;
 
     SOUTH_TX_SETUP;
     // return signal to default (high on receiver side)
