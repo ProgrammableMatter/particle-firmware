e25f31cc5e6618f37232c7e5f4e34e72017bc9b7
Raoul Rubien <rubienr@gmail.com>
2016-06-21 19:00:21 +0200

    simplified/cleaned up particle configuration parameters

--- src/avr/avr-common/utils/uc-core/fw-configuration/ParticleParameters.h
+++ src/avr/avr-common/utils/uc-core/fw-configuration/ParticleParameters.h
@@ -33,54 +33,17 @@
 #define DEFAULT_NEIGHBOUR_SENSING_COUNTER_COMPARE_VALUE ((uint16_t)0x80)
 
 /**
- * Initial value for reception counter compare value. Should be greater than 450 and less than 2032. Note
- * that (2*DEFAULT_TX_RX_COMPARE_TOP_VALUE + 2*ParticleAttributes.rxArguments.receptionDelta) must
- * fit into uint16_t.
+ * Initial value for clock delay for Manchester (de-)coding (reception and transmission).
  */
-#define DEFAULT_TX_RX_COMPARE_TOP_VALUE ((uint16_t) 2000)
-
+#define DEFAULT_TX_RX_CLOCK_DELAY ((uint16_t) 2000)
 
 /**
  * Maximum short and long time lag. If max. long snapshot lag is exceeded the reception
  * experiences a timeout.
  *
  */
-#define DEFAULT_MAX_LONG_RECEPTION_SNAPSHOT_DISTANCE ((uint16_t)(1.04 * DEFAULT_TX_RX_COMPARE_TOP_VALUE))
-#define DEFAULT_MAX_SHORT_RECEPTION_SNAPSHOT_DISTANCE ((uint16_t)(0.54 * DEFAULT_TX_RX_COMPARE_TOP_VALUE))
-
-/**
- * Accepted deviation (constant) on reception:
- * Signals received within [DEFAULT_TX_RX_COMPARE_TOP_VALUE +/- TX_RX_RECEPTION_DELTA_DIVISOR ] time span are
- * classified synchronization/rectification bits.
- * Signals received within [DEFAULT_TX_RX_COMPARE_TOP_VALUE / DEFAULT_TX_RX_COUNTER_CENTER_VALUE_DIVISOR +/- TX_RX_RECEPTION_DELTA_DIVISOR ] time span are classified as data bits.
- */
-#define TX_RX_RECEPTION_DELTA_VALUE_DIVISOR 4
-
-/**
- * Divisor for calculating the center of DEFAULT_TX_RX_COMPARE_TOP_VALUE.
- */
-#define TX_RX_COUNTER_CENTER_VALUE_DIVISOR 2
-
-/**
- * The center interrupt is pre-scheduled by the offset to adjust the interrupt latency.
- */
-#define TX_RX_COUNTER_CENTER_INTERRUPT_LATENCY_ADJUSTMENT 0
-
-
-/**
- * The left, center and right borders are classification limits which are used to determine whether a received
- * signal represents a data bit or is the coding clock/rectification flank.
- */
-#define TX_RX_RECEPTION_CLASSIFICATION_VALUE_LEFT_BORDER ((uint16_t)(DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_RECEPTION_DELTA_VALUE_DIVISOR))
-#define TX_RX_RECEPTION_CLASSIFICATION_VALUE_CENTER ((uint16_t)(DEFAULT_TX_RX_COMPARE_TOP_VALUE / TX_RX_COUNTER_CENTER_VALUE_DIVISOR))
-#define TX_RX_RECEPTION_CLASSIFICATION_VALUE_RIGHT_BORDER ((uint16_t)(TX_RX_RECEPTION_CLASSIFICATION_VALUE_CENTER + TX_RX_RECEPTION_CLASSIFICATION_VALUE_LEFT_BORDER))
-
-
-/**
- * Initial counter compare interrupt value for updating the ongoing reception timeout.
- */
-// TODO: timeout has been removed from timer/counter0 interrupt -> remove timeout related macros
-#define RX_TIMEOUT_INTERRUPT_VALUE_DIVISOR 8
+#define DEFAULT_MAX_SHORT_RECEPTION_OVERTIME_PERCENTAGE_RATIO ((uint8_t) 54)
+#define DEFAULT_MAX_LONG_RECEPTION_OVERTIME_PERCENTAGE_RATIO ((uint8_t) 104)
 
 /**
  * Heartbeat LED toggles every main-loop count
