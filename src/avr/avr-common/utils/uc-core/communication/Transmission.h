f000415672deee0d778ed070e8822cb9b4c72b63
Raoul Rubien <rubienr@gmail.com>
2016-06-21 19:03:57 +0200

    begun tx strategy refactoring

--- src/avr/avr-common/utils/uc-core/communication/Transmission.h
+++ src/avr/avr-common/utils/uc-core/communication/Transmission.h
@@ -5,8 +5,22 @@
 
 #pragma once
 
-#ifdef SIMULATION
+#include "uc-core/interrupts/TimerCounter.h"
 
-#  include "simulation/SimulationMacros.h"
+FUNC_ATTRS void scheduleNextTransmissionInterrupt(void) {
 
-#endif
+    TIMER_TX_RX_COMPARE_VALUE = TIMER_TX_RX_COUNTER %
+                                ParticleAttributes.ports.timerAdjustment.transmissionClockDelay
+                                + 2 * ParticleAttributes.ports.timerAdjustment.transmissionClockDelay;
+    // TODO
+    // schedule transmission:
+    //  calculate next transmission slot
+    //  set compareA value
+
+}
+
+FUNC_ATTRS void enableTransmission(volatile TxPort *port) {
+    scheduleNextTransmissionInterrupt();
+    port->isTransmitting = true;
+    // TODO
+}
