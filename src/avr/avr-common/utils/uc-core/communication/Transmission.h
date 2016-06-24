f4e4aaeb8e2ac93e2c8dbc7a68038607aca8e868
Raoul Rubien <rubienr@gmail.com>
2016-06-24 12:31:44 +0200

    transmission start starts now two tx clocks in future instead of one clock due to faster tx clock setting (isr overlapping)

--- src/avr/avr-common/utils/uc-core/communication/Transmission.h
+++ src/avr/avr-common/utils/uc-core/communication/Transmission.h
@@ -13,9 +13,9 @@ extern FUNC_ATTRS void scheduleNextTxInterrupt(void);
  * Schedules the next transmission interrupt.
  */
 FUNC_ATTRS void scheduleNextTxInterrupt(void) {
-    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
+//    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
     TIMER_TX_RX_COMPARE_VALUE += ParticleAttributes.ports.timerAdjustment.transmissionClockDelayHalf;
-    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
+//    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
 }
 
 extern FUNC_ATTRS void scheduleStartTxInterrupt(void);
@@ -24,11 +24,11 @@ extern FUNC_ATTRS void scheduleStartTxInterrupt(void);
  */
 FUNC_ATTRS void scheduleStartTxInterrupt(void) {
     uint16_t counter = TIMER_TX_RX_COUNTER_VALUE;
-    DEBUG_INT16_OUT(counter);
+//    DEBUG_INT16_OUT(counter);
     TIMER_TX_RX_COMPARE_VALUE = counter - (counter %
                                            ParticleAttributes.ports.timerAdjustment.transmissionClockDelay)
-                                + ParticleAttributes.ports.timerAdjustment.transmissionClockDelay;
-    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
+                                + 2 * ParticleAttributes.ports.timerAdjustment.transmissionClockDelay;
+//    DEBUG_INT16_OUT(TIMER_TX_RX_COMPARE_VALUE);
     TIMER_TX_RX_ENABLE_COMPARE_INTERRUPT;
 }
 
@@ -39,5 +39,6 @@ extern FUNC_ATTRS void enableTransmission(volatile TxPort *port);
 FUNC_ATTRS void enableTransmission(volatile TxPort *port) {
     port->isTxClockPhase = true;
     port->isTransmitting = true;
+    port->isDataBuffered = true;
     scheduleStartTxInterrupt();
 }
