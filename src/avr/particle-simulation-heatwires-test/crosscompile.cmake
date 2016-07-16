3a96c8e3ccd9cefffb7e5758aaf8f3675e968771
Raoul Rubien <rubienr@gmail.com>
2016-07-16 15:24:47 +0200

    added test-able heat wires command related binary generating folder to project

---
+++ src/avr/particle-simulation-heatwires-test/crosscompile.cmake
@@ -0,0 +1,7 @@
+# @author Raoul Rubien 16.07.2016
+
+include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/cpu_clock_8000000.cmake)
+include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/cpu_m16.cmake)
+SET(DEFINED_MACROS "-DSIMULATION=true ${DEFINED_MACROS}")
+include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_debug.cmake)
+include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_global.cmake)
---
+++ src/avr/particle-simulation-heatwires-test/main/main.c
@@ -0,0 +1,12 @@
+/**
+ * @author Raoul Rubien 2015
+ */
+
+#define SIMULATION_HEAT_WIRES_TEST
+
+#include <uc-core/particle/ParticleLoop.h>
+
+int main(void) {
+    particleLoop();
+    return 0;
+}
