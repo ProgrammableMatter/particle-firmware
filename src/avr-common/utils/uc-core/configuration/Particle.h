05d85261c1ffffed518f2ca2c9c20526c9285bc1
Raoul Rubien <rubienr@gmail.com>
2016-09-22 00:09:40 +0200

    tuned for real hardware

--- src/avr-common/utils/uc-core/configuration/Particle.h
+++ src/avr-common/utils/uc-core/configuration/Particle.h
@@ -9,14 +9,14 @@
 /**
  * Delay separating particle loops while in discovery state.
  */
-//#define PARTICLE_DISCOVERY_LOOP_DELAY
-//    DELAY_US_30
+#define PARTICLE_DISCOVERY_LOOP_DELAY \
+    DELAY_US_30
 
 /**
  * Delay separating the following state switch after discovery has finished.
  */
 #define PARTICLE_DISCOVERY_PULSE_DONE_POST_DELAY \
-    DELAY_US_500; \
-    DELAY_US_500; \
-    DELAY_US_500; \
+    DELAY_MS_1; \
+    DELAY_MS_1; \
+    DELAY_MS_1; \
     DELAY_US_500
