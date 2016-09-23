f4a78ed3a93d5196acd8f508918293084280e437
Raoul Rubien <rubienr@gmail.com>
2016-09-23 23:21:54 +0200

    enhancd optimize for size avr-gcc args

--- src/particle/crosscompile.cmake
+++ src/particle/crosscompile.cmake
@@ -4,10 +4,8 @@ include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/cpu_clock_8000000.cmake)
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/cpu_attiny1634.cmake)
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/programmer_target_t1634.cmake)
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_release.cmake)
-# for simulation purpose
-#SET(COPT "-Os -fno-inline-small-functions -fno-inline")
-#SET(COPT "-O1")  # for simulation purpose
-# for real MCU
+SET(COPT "-Os -fwhole-program -fno-inline-small-functions")
 #SET(COPT "-O3 -fwhole-program")
-SET(COPT "-O2 -fwhole-program")
+#SET(COPT "-O2 -fwhole-program")
+#SET(COPT "-O1 -fwhole-program")
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_global.cmake)
