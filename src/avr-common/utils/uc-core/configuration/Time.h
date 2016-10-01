6f461d073042a09b139fa9132b06dbaaad65cf86
Raoul Rubien <rubienr@gmail.com>
2016-10-01 14:50:41 +0200

    enhanced configuration to be more readable

--- src/avr-common/utils/uc-core/configuration/Time.h
+++ src/avr-common/utils/uc-core/configuration/Time.h
@@ -6,8 +6,14 @@
 
 #pragma once
 
+#define __LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_75_PERCENT_OF_UINT16_MAX ((uint16_t) 49151)
+#define __LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_80_PERCENT_OF_UINT16_MAX ((uint16_t) 52428)
+#define __LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_85_PERCENT_OF_UINT16_MAX ((uint16_t) 55704)
+#define __LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_90_PERCENT_OF_UINT16_MAX ((uint16_t) 58982)
+
+
 /**
  * The default start value for time compare match interrupt.
  * Defines the duration of time periods which can be adjusted at run time.
  */
-#define LOCAL_TIME_DEFAULT_INTERRUPT_DELAY ((uint16_t) 65000)
+#define LOCAL_TIME_DEFAULT_INTERRUPT_DELAY __LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_80_PERCENT_OF_UINT16_MAX
