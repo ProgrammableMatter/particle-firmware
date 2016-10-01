0de332bd9ec6bd58a68483a6d85735c98c85e5c5
Raoul Rubien <rubienr@gmail.com>
2016-10-01 14:45:21 +0200

    attiny1634 only: configured clkout in fuses

--- src/particle/main/main.c
+++ src/particle/main/main.c
@@ -5,7 +5,7 @@
 #include <uc-core/particle/ParticleLoop.h>
 
 FUSES = {
-        .low =  (FUSE_SUT_CKSEL0 & FUSE_SUT_CKSEL2 & FUSE_SUT_CKSEL3 & FUSE_SUT_CKSEL4),
+        .low =  (FUSE_SUT_CKSEL0 & FUSE_SUT_CKSEL2 & FUSE_SUT_CKSEL3 & FUSE_SUT_CKSEL4 & FUSE_CKOUT),
         .high = HFUSE_DEFAULT,
         .extended = EFUSE_DEFAULT,
 };
