56e093cc7e7d765f35f3ad7945610c95217ef6a8
Raoul Rubien <rubienr@gmail.com>
2016-10-01 14:51:28 +0200

    minor change

--- src/avr-common/utils/uc-core/time/Time.h
+++ src/avr-common/utils/uc-core/time/Time.h
@@ -10,20 +10,23 @@
 /**
  * Schedules the next time interrupt.
  */
-void scheduleNextLocalTimeInterrupt(void) {
-    if (ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable) {
-        ParticleAttributes.localTime.timePeriodInterruptDelay = ParticleAttributes.localTime.newTimePeriodInterruptDelay;
-        MEMORY_BARRIER;
-        ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable = false;
-    }
-    LOCAL_TIME_INTERRUPT_COMPARE_VALUE += ParticleAttributes.localTime.timePeriodInterruptDelay;
-}
+//void scheduleNextLocalTimeInterrupt(void) {
+//    if (ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable) {
+//        ParticleAttributes.localTime.timePeriodInterruptDelay = ParticleAttributes.localTime.newTimePeriodInterruptDelay;
+//        MEMORY_BARRIER;
+//        ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable = false;
+//    }
+//    LOCAL_TIME_INTERRUPT_COMPARE_VALUE += ParticleAttributes.localTime.timePeriodInterruptDelay;
+//}
 
 /**
- * enables the local time interrupt using current adjustment argument
+ * Enables the local time interrupt using current adjustment argument
  * {@link ParticleAttributes.localTime.timePeriodInterruptDelay}.
+ * Write to LOCAL_TIME_INTERRUPT_COMPARE_VALUE is not atomic.
  */
 void enableLocalTimeInterrupt(void) {
+    LOCAL_TIME_INTERRUPT_COMPARE_DISABLE;
+    MEMORY_BARRIER;
     LOCAL_TIME_INTERRUPT_COMPARE_VALUE = ParticleAttributes.localTime.timePeriodInterruptDelay;
     MEMORY_BARRIER;
     LOCAL_TIME_INTERRUPT_COMPARE_ENABLE;
