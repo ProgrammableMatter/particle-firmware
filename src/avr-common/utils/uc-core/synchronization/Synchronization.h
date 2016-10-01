187e4a5ce18700c5409b9f8844a62035951ee892
Raoul Rubien <rubienr@gmail.com>
2016-10-01 17:10:38 +0200

    implemented local time tracking interval duration approximation

--- src/avr-common/utils/uc-core/synchronization/Synchronization.h
+++ src/avr-common/utils/uc-core/synchronization/Synchronization.h
@@ -38,35 +38,34 @@ static void __roundFloatToUnsigned(volatile const float *const source, volatile 
  * it's pdu reception duration offset has been stored to the fifo queue.
  */
 void tryApproximateTimings(TimeSynchronization *const timeSynchronization) {
-    if (false == __isEnoughDataAvailable(timeSynchronization)) {
-        return;
-    }
+    if (__isEnoughDataAvailable(timeSynchronization)) {
+        if (ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable == false) {
 
 #ifdef SYNCHRONIZATION_STRATEGY_SIMPLE_MEAN_AND_STDDEVIANCE
-    if (timeSynchronization->progressiveMean == 0) {
-        timeSynchronization->progressiveMean = timeSynchronization->timeIntervalSamples.samples[timeSynchronization->timeIntervalSamples.__insertIndex];
-    } else {
-        timeSynchronization->progressiveMean +=
-                timeSynchronization->timeIntervalSamples.samples[timeSynchronization->timeIntervalSamples.__insertIndex];
-        timeSynchronization->progressiveMean /= 2.0;
-    }
-
-    calculateMean(timeSynchronization);
-    calculateVarianceAndStdDeviance(timeSynchronization);
+            calculateMean(timeSynchronization);
+            calculateVarianceAndStdDeviance(timeSynchronization);
 #else
 #  if defined(SYNCHRONIZATION_STRATEGY_LEAST_SQUARE_LINEAR_FITTING)
-    calculateLinearFittingFunction(timeSynchronization);
+            calculateLinearFittingFunctionVarianceAndStdDeviance(timeSynchronization);
 #  else
 #    error no approximation strategy specified
 #  endif
 #endif
-
-    if (__isEnoughDataAvailable(timeSynchronization)) {
-        if (ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable == false) {
-            // remove synthetic added offset from mean and round properly
-            float ratio = (ParticleAttributes.timeSynchronization.mean / TIME_SYNCHRONIZATION_SAMPLE_OFFSET);
-            ratio = ratio * LOCAL_TIME_DEFAULT_INTERRUPT_DELAY;
-            __roundFloatToUnsigned(&ratio, &ParticleAttributes.localTime.newTimePeriodInterruptDelay);
+            float newTimePeriodInterruptDelay;
+            if (ParticleAttributes.timeSynchronization.mean > TIME_SYNCHRONIZATION_SAMPLE_OFFSET) {
+                newTimePeriodInterruptDelay = LOCAL_TIME_DEFAULT_INTERRUPT_DELAY +
+                                              LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_WORKING_POINT_PERCENTAGE *
+                                              (ParticleAttributes.timeSynchronization.mean -
+                                               TIME_SYNCHRONIZATION_SAMPLE_OFFSET);
+            } else {
+                newTimePeriodInterruptDelay = LOCAL_TIME_DEFAULT_INTERRUPT_DELAY -
+                                              LOCAL_TIME_DEFAULT_INTERRUPT_DELAY_WORKING_POINT_PERCENTAGE *
+                                              (TIME_SYNCHRONIZATION_SAMPLE_OFFSET -
+                                               ParticleAttributes.timeSynchronization.mean);
+            }
+            __roundFloatToUnsigned(&newTimePeriodInterruptDelay,
+                                   &ParticleAttributes.localTime.newTimePeriodInterruptDelay);
+            ParticleAttributes.localTime.isTimePeriodInterruptDelayUpdateable = true;
         }
     }
 }
