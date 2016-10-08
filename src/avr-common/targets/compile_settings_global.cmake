596b9b4b62fa29d244b98b7a0e1c37bcda8d4731
Raoul Rubien <rubienr@gmail.com>
2016-10-08 12:04:14 +0200

    extended printf library flags for linker otherwise float is printed as ?

--- src/avr-common/targets/compile_settings_global.cmake
+++ src/avr-common/targets/compile_settings_global.cmake
@@ -13,7 +13,9 @@ SET(CWARN "-Wall -Werror -Wvolatile-register-var -Wunused-variable -Wunused-func
 SET(CXXWARN "-Wall -Werror -Wvolatile-register-var -Wunused-variable -Wunused-function -Wunused-label -Wunused-value -Wextra -Wshadow -Wstrict-prototypes -Waddress -Wredundant-decls")
 
 # Do not use "-Wl,--gc-sections"!
-SET(CTUNING " -fpack-struct -funsigned-bitfields -funsigned-char -fdce -fdata-sections -ffunction-sections ${CTUNING}")
+SET(CTUNING " -fpack-struct -funsigned-bitfields -funsigned-char -fdata-sections -fdce -ffunction-sections ${CTUNING}")
+
+SET(PRINTF_LIB_FLOAT "${PRINTF_LIB_FLOAT}")
 
 SET(COPT "${COPT}")
 SET(CDEBUG "${CDEBUG}")
@@ -21,8 +23,8 @@ SET(CDEBUG "${CDEBUG}")
 SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
 SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
 
-SET(CFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${COPT} ${CWARN} ${CSTANDARD} ${CTUNING} ${DEFINED_MACROS}")
-SET(CXXFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${COPT} ${CXXWARN} ${CXXSTANDARD} ${CTUNING} ${DEFINED_MACROS}")
+SET(CFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${PRINTF_LIB_FLOAT} ${COPT} ${CWARN} ${CSTANDARD} ${CTUNING} ${DEFINED_MACROS}")
+SET(CXXFLAGS "${CMCU} ${CDEBUG} ${CDEFS} ${CINCS} ${PRINTF_LIB_FLOAT}${COPT} ${CXXWARN} ${CXXSTANDARD} ${CTUNING} ${DEFINED_MACROS}")
 
 SET(CMAKE_C_FLAGS ${CFLAGS})
 SET(CMAKE_CXX_FLAGS ${CXXFLAGS})
--- src/particle/crosscompile.cmake
+++ src/particle/crosscompile.cmake
@@ -6,4 +6,7 @@ include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/programmer_target_t1634.cmake
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_release.cmake)
 #SET(COPT "-O1 -fwhole-program")
 SET(COPT "-Os -fwhole-program -fno-inline-small-functions")
+
+#SET(PRINTF_LIB_FLOAT "-Wl,-u,vfprintf -lprintf_flt")
+SET(PRINTF_LIB_FLOAT "")
 include(${PROJECTS_SOURCE_ROOT}/avr-common/targets/compile_settings_global.cmake)
