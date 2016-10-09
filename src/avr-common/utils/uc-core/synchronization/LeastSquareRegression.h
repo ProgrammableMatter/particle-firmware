772b3f2c8b3f3eb61facfef570c68e860254acb9
Raoul Rubien <rubienr@gmail.com>
2016-10-09 13:48:37 +0200

    fixed issue with missing left-hand cast where parts of calculation were implicitly casted to small data types which caused overflows

--- src/avr-common/utils/uc-core/synchronization/LeastSquareRegression.h
+++ src/avr-common/utils/uc-core/synchronization/LeastSquareRegression.h
@@ -19,22 +19,29 @@ void calculateLinearFittingFunctionVarianceAndStdDeviance(TimeSynchronization *c
     timeSynchronization->fittingFunction.isCalculationValid = false;
     MEMORY_BARRIER;
 
-//    if (false == __isEnoughDataAvailable(timeSynchronization)) {
-//        return;
-//    }
-
     CumulationType a_ = 0, b_ = 0, c_ = 0, e_ = 0;
     SampleValueType y = 0;
     IndexType x = 1;
-    timeSynchronization->mean = 0;
+
+//    cli();
+//    samplesFifoBufferIteratorStart(&timeSynchronization->timeIntervalSamples);
+//    do {
+//        printf("%u,\n",
+//               timeSynchronization->timeIntervalSamples.samples[timeSynchronization->timeIntervalSamples.iterator].value);
+//        samplesFifoBufferFiFoBufferIteratorNext(&timeSynchronization->timeIntervalSamples);
+//    } while (timeSynchronization->timeIntervalSamples.iterator <
+//             TIME_SYNCHRONIZATION_SAMPLES_FIFO_BUFFER_ITERATOR_END);
+
 
     samplesFifoBufferIteratorStart(&timeSynchronization->timeIntervalSamples);
     do {
-        y = timeSynchronization->timeIntervalSamples.samples[timeSynchronization->timeIntervalSamples.iterator];
+        y = timeSynchronization->timeIntervalSamples.samples[timeSynchronization->timeIntervalSamples.iterator].value;
+        // printf("d%u\n", y);
         // a_ += 2 * x * x;
-        a_ += x * x;
+        a_ += (CumulationType) x * x;
+
         // b_ += 2 * x * y;
-        b_ += x * y;
+        b_ += (CumulationType) x * y;
         // c_ += 2 * x;
         c_ += x;
         // d_ += 2; --> same as numSamples * 2
@@ -42,7 +49,8 @@ void calculateLinearFittingFunctionVarianceAndStdDeviance(TimeSynchronization *c
         // f += 2 * x; --> f == c
 
         // 2-factor pulled our of loop means each factor *= 2
-        timeSynchronization->mean += y;
+
+        // printf("[%u] --> a %lu b %lu c %lu e %lu\n", x, a_, b_, c_, e_);
         x++;
 
         samplesFifoBufferFiFoBufferIteratorNext(&timeSynchronization->timeIntervalSamples);
@@ -56,15 +64,17 @@ void calculateLinearFittingFunctionVarianceAndStdDeviance(TimeSynchronization *c
     e_ *= 2;
     CumulationType *f_ = &c_;
 
-    timeSynchronization->mean =
-            timeSynchronization->mean / (CalculationType) timeSynchronization->timeIntervalSamples.numSamples;
-    calculateVarianceAndStdDeviance(timeSynchronization);
+    // printf("a%lu b%lu c%lu d%u e%lu f%lu\n", a_, b_, c_, d_, e_, *f_);
 
     // we obtain two linear equations from the transformed partial derivatives to d and k
     // with k explicitly
-    timeSynchronization->fittingFunction.k = (d_ * b_ - c_ * e_) / (CalculationType) (d_ * a_ - (*f_) * c_);
+    timeSynchronization->fittingFunction.k =
+            ((CalculationType) d_ * b_ - (CalculationType) c_ * e_) / (d_ * a_ - (*f_) * c_);
     // and d explicitly
     timeSynchronization->fittingFunction.d = (timeSynchronization->fittingFunction.k * a_ + b_) / c_;
+
+    // printf("k%li d%li\n", (int32_t) timeSynchronization->fittingFunction.k,
+    //   (int32_t) timeSynchronization->fittingFunction.d);
 
     MEMORY_BARRIER;
     timeSynchronization->fittingFunction.isCalculationValid = true;
