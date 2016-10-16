1a629bf49ba54e103cd827278bc9a1b2d64365d1
Raoul Rubien <rubienr@gmail.com>
2016-10-16 16:17:41 +0200

    enhanced planning of sync. and heat wires command tasks

--- src/avr-common/utils/uc-core/evaluation/Evaluation.h
+++ src/avr-common/utils/uc-core/evaluation/Evaluation.h
@@ -7,28 +7,33 @@
 #pragma once
 
 #include "uc-core/particle/Globals.h"
-//#include "EvaluationTypes.h"
 
+void heatWires(SchedulerTask *const task);
+
+/**
+ * initiates sending the next synchronization package
+ */
 void sendNextSyncTimePackage(SchedulerTask *const task) {
-    LED_STATUS2_TOGGLE;
-    if (ParticleAttributes.timeSynchronization.isNextSyncPackageTransmissionEnabled) {
-        if (ParticleAttributes.timeSynchronization.totalFastSyncPackagesToTransmit <= 0) {
-            // separation on default synchronization
-            task->reScheduleDelay = ParticleAttributes.timeSynchronization.syncPackageSeparation;
-        } else {
-            // separation on fast synchronization
-            task->reScheduleDelay = ParticleAttributes.timeSynchronization.fastSyncPackageSeparation;
-            ParticleAttributes.timeSynchronization.totalFastSyncPackagesToTransmit--;
+    if (false == task->isLastCall) {
+        if (ParticleAttributes.timeSynchronization.isNextSyncPackageTransmissionEnabled) {
+            if (ParticleAttributes.timeSynchronization.totalFastSyncPackagesToTransmit <= 0) {
+                // separation on default synchronization
+                task->reScheduleDelay = ParticleAttributes.timeSynchronization.syncPackageSeparation;
+            } else {
+                // separation on fast synchronization
+                task->reScheduleDelay = ParticleAttributes.timeSynchronization.fastSyncPackageSeparation;
+                ParticleAttributes.timeSynchronization.totalFastSyncPackagesToTransmit--;
+            }
+            LED_STATUS2_TOGGLE;
+            ParticleAttributes.node.state = STATE_TYPE_RESYNC_NEIGHBOUR;
+            ParticleAttributes.timeSynchronization.isNextSyncPackageTransmissionEnabled = false;
+            ParticleAttributes.protocol.isSimultaneousTransmissionEnabled = true;
         }
-        ParticleAttributes.node.state = STATE_TYPE_RESYNC_NEIGHBOUR;
-        ParticleAttributes.timeSynchronization.isNextSyncPackageTransmissionEnabled = false;
-    }
-
-    if (task->isLastCall) {
+    } else {
         // on last call enable heat wires task
-        ParticleAttributes.scheduler.tasks[SCHEDULER_TASK_ID_HEAT_WIRES].startTimestamp =
-                task->startTimestamp + 500;
-        enableTask(SCHEDULER_TASK_ID_HEAT_WIRES);
+        addCyclicTask(SCHEDULER_TASK_ID_HEAT_WIRES, heatWires, task->startTimestamp + 400, 200);
+        taskEnableNodeTypeLimit(SCHEDULER_TASK_ID_HEAT_WIRES, NODE_TYPE_ORIGIN);
+        taskEnable(SCHEDULER_TASK_ID_HEAT_WIRES);
     }
 }
 
@@ -39,7 +44,7 @@ static bool __incrementNextHeatWiresAddress(void) {
     } else {
         if (ParticleAttributes.evaluation.nextHeatWiresAddress.column <
             ParticleAttributes.protocol.networkGeometry.columns) {
-            ParticleAttributes.evaluation.nextHeatWiresAddress.row = 0;
+            ParticleAttributes.evaluation.nextHeatWiresAddress.row = 2;
             ParticleAttributes.evaluation.nextHeatWiresAddress.column++;
         } else {
             return false;
@@ -48,26 +53,30 @@ static bool __incrementNextHeatWiresAddress(void) {
     return true;
 }
 
+/**
+ * initiates sending next actuation (heat wires) command
+ */
 void heatWires(SchedulerTask *const task) {
     if (__incrementNextHeatWiresAddress()) {
+        // on having new address to traverse
         Actuators actuators;
         actuators.northLeft = true;
         actuators.northRight = true;
         NodeAddress nodeAddress;
-        nodeAddress.row = 3;
-        nodeAddress.column = 1;
-        sendHeatWires(&nodeAddress, &actuators, task->startTimestamp + 500, 500);
-        task->isEnabled = false;
+        nodeAddress.row = ParticleAttributes.evaluation.nextHeatWiresAddress.row;
+        nodeAddress.column = ParticleAttributes.evaluation.nextHeatWiresAddress.column;
+        sendHeatWires(&nodeAddress, &actuators, task->startTimestamp + 50, 100);
+    }
+    else {
+        // on no more address to traverse: enable cyclic sync. pkg.
+        ParticleAttributes.evaluation.nextHeatWiresAddress.row = 1;
+        ParticleAttributes.evaluation.nextHeatWiresAddress.column = 1;
 
-        // re-enable send sync pkg. task
-        ParticleAttributes.scheduler.tasks[SCHEDULER_TASK_ID_SYNC_PACKAGE].startTimestamp =
-                task->startTimestamp + 1500;
-        setCountLimitedTask(SCHEDULER_TASK_ID_SYNC_PACKAGE, 20);
-        enableTask(SCHEDULER_TASK_ID_SYNC_PACKAGE);
-    } else {
-        ParticleAttributes.scheduler.tasks[SCHEDULER_TASK_ID_SYNC_PACKAGE].startTimestamp =
-                task->startTimestamp + 500;
-        ParticleAttributes.scheduler.tasks[SCHEDULER_TASK_ID_SYNC_PACKAGE].isCountLimitedTask = false;
-        disableTask(SCHEDULER_TASK_ID_HEAT_WIRES);
+        addCyclicTask(SCHEDULER_TASK_ID_SYNC_PACKAGE, sendNextSyncTimePackage, task->startTimestamp + 100,
+                      100);
+        taskEnableNodeTypeLimit(SCHEDULER_TASK_ID_SYNC_PACKAGE, NODE_TYPE_ORIGIN);
+        taskEnableCountLimit(SCHEDULER_TASK_ID_SYNC_PACKAGE, 20);
+        taskEnable(SCHEDULER_TASK_ID_SYNC_PACKAGE);
+        taskDisable(SCHEDULER_TASK_ID_HEAT_WIRES);
     }
 }
