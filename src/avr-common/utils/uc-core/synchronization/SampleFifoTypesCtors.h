79355cb1fc6fd7bba15e535fb72c5171c08516e1
Raoul Rubien <rubienr@gmail.com>
2016-10-28 11:46:07 +0200

    fifo is now initialized wit default values to speed up clock skew sync. delay

--- src/avr-common/utils/uc-core/synchronization/SampleFifoTypesCtors.h
+++ src/avr-common/utils/uc-core/synchronization/SampleFifoTypesCtors.h
@@ -10,7 +10,12 @@
 #include "LeastSquareRegressionTypesCtors.h"
 
 void constructFifoElement(FifoElement *const o) {
-    o->value = 0;
+    // The lightweight FiFo works only if full:
+    // One can wait until enough observations have been stored to the queue
+    // or speed up by pre-filling with synthetic values.
+    o->value = (uint16_t) (roundf(SYNCHRONIZATION_PDU_NUMBER_CLOCKS_IN_MEASURED_INTERVAL *
+                                  COMMUNICATION_DEFAULT_TX_RX_CLOCK_DELAY))
+               - TIME_SYNCHRONIZATION_SAMPLE_OFFSET;
     o->isRejected = false;
 }
 
@@ -24,7 +29,9 @@ void constructSamplesFifoBuffer(SamplesFifoBuffer *const o) {
     }
     o->__startIdx = 0;
     o->__insertIndex = 0;
-    o->numSamples = 0;
+    // workaround for pre-filling with synthetic values: indicate that FiFo is full
+    o->numSamples = SAMPLE_FIFO_NUM_BUFFER_ELEMENTS;
+    // o->numSamples = 0;
     o->iterator = o->__startIdx;
     constructFifoElement(&o->dropOut);
     o->isDropOutValid = false;
