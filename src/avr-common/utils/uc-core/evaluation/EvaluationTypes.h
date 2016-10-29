ab740cfd33549b5252658f0709c5fac2731d7039
Raoul Rubien <rubienr@gmail.com>
2016-10-29 14:22:11 +0200

    cleanup

--- src/avr-common/utils/uc-core/evaluation/EvaluationTypes.h
+++ src/avr-common/utils/uc-core/evaluation/EvaluationTypes.h
@@ -7,9 +7,40 @@
 #pragma once
 
 #include <stdint.h>
-
 #include "uc-core/particle/types/NodeAddressTypes.h"
+#include "uc-core/configuration/Evaluation.h"
+
+#ifdef EVALUATION_ENABLE_FLUCTUATE_CPU_FREQUENCY_ON_PURPOSE
+typedef struct RuntimeOscCalibration {
+    uint8_t initialOscCal;
+    uint8_t step;
+    uint8_t minOscCal;
+    uint8_t maxOscCal;
+    uint8_t isDecrementing : 1;
+    uint8_t __pad : 7;
+
+} RuntimeOscCalibration;
+#endif
 
 typedef struct Evaluation {
+#if defined(EVALUATION_SIMPLE_SYNC_AND_ACTUATION) || defined(EVALUATION_SYNC_CYCLICALLY)
+    /**
+     * network address to issue next heat wires command at
+     **/
     NodeAddress nextHeatWiresAddress;
+#endif
+
+#ifdef EVALUATION_ENABLE_FLUCTUATE_CPU_FREQUENCY_ON_PURPOSE
+    /**
+     * OSCCAL runtime boundaries
+     */
+    RuntimeOscCalibration oscCalibration;
+#endif
+
+#ifdef EVALUATION_SYNC_WITH_CYCLIC_UPDATE_TIME_REQUEST_FLAG_THEN_ACTUATE_ONCE
+    /**
+     * total number of currently sent sync packages
+     */
+    uint16_t totalSentSyncPackages;
+#endif
 } Evaluation;
--- src/avr-common/utils/uc-core/evaluation/EvaluationTypesCtors.h
+++ src/avr-common/utils/uc-core/evaluation/EvaluationTypesCtors.h
@@ -7,12 +7,46 @@
 #pragma once
 
 #include "EvaluationTypes.h"
+#include "uc-core/configuration/Evaluation.h"
+
+#ifdef EVALUATION_ENABLE_FLUCTUATE_CPU_FREQUENCY_ON_PURPOSE
+/**
+ * constructor function
+ * @param o the object to construct
+ **/
+void constructRuntimeOscCalibration(RuntimeOscCalibration *const o) {
+    o->initialOscCal = EVALUATION_OSC_CALIBRATION_REGISTER;
+    o->step = EVALUATION_OSCCAL_STEP;
+    o->minOscCal = o->initialOscCal;
+    o->maxOscCal = o->initialOscCal;
+
+    if (o->initialOscCal > EVALUATION_OSCCAL_MIN_OFFSET) {
+        o->minOscCal = o->initialOscCal - EVALUATION_OSCCAL_MIN_OFFSET;
+    }
+
+    if ((UINT8_MAX - EVALUATION_OSCCAL_MAX_OFFSET) > o->initialOscCal) {
+        o->maxOscCal = o->initialOscCal + EVALUATION_OSCCAL_MIN_OFFSET;
+    }
+
+    o->isDecrementing = false;
+}
+#endif
 
 /**
-* constructor function
-* @param o the object to construct
-**/
+ * constructor function
+ * @param o the object to construct
+ **/
 void constructEvaluation(Evaluation *const o) {
+#if defined(EVALUATION_SIMPLE_SYNC_AND_ACTUATION) || defined(EVALUATION_SYNC_CYCLICALLY)
     o->nextHeatWiresAddress.row = 1;
     o->nextHeatWiresAddress.column = 1;
+#endif
+
+#ifdef EVALUATION_ENABLE_FLUCTUATE_CPU_FREQUENCY_ON_PURPOSE
+    constructRuntimeOscCalibration(&o->oscCalibration);
+#endif
+
+#ifdef EVALUATION_SYNC_WITH_CYCLIC_UPDATE_TIME_REQUEST_FLAG_THEN_ACTUATE_ONCE
+    o->totalSentSyncPackages = 0;
+#endif
 }
